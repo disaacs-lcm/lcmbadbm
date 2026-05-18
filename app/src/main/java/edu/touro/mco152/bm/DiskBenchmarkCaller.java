39c3e764dffb760b4867e9826b146cd13aa8c6a5
llamalectric <lotsnlotsofllamas@gmail.com>
2026-05-18 09:01:07 -0400

    Add javadoc to DiskBenchmarkCaller and DiskUI

--- app/src/main/java/edu/touro/mco152/bm/DiskBenchmarkCaller.java
+++ app/src/main/java/edu/touro/mco152/bm/DiskBenchmarkCaller.java
@@ -1,10 +1,28 @@
 package edu.touro.mco152.bm;
 
+/**
+ * Any background task that runs {@link DiskBenchmarker}.
+ * It uses this interface to report progress, publish results, and check for
+ * cancellation without depending on SwingWorker.
+ */
 public interface DiskBenchmarkCaller {
 
+	/**
+	 * Returns true if the user has requested to cancel the benchmark.
+	 */
 	boolean isCancelled();
 
+	/**
+	 * Reports how far along the benchmark is.
+	 *
+	 * @param progress a value from 0 to 100
+	 */
 	void setBenchmarkProgress(int progress);
 
+	/**
+	 * Sends completed benchmark results to the UI thread for display.
+	 *
+	 * @param chunks one or more DiskMark results to publish
+	 */
 	void publishChunks(DiskMark... chunks);
 }
--- app/src/main/java/edu/touro/mco152/bm/DiskUI.java
+++ app/src/main/java/edu/touro/mco152/bm/DiskUI.java
@@ -2,23 +2,63 @@ package edu.touro.mco152.bm;
 
 import edu.touro.mco152.bm.persist.DiskRun;
 
+/**
+ * Represents the UI that {@link DiskBenchmarker} can interact with.
+ * Any class that implements this interface can be used as the UI for a
+ * benchmark run.
+ */
 public interface DiskUI {
 
+	/**
+	 * Inits the chart and sets the title to the given text.
+	 *
+	 * @param titleText the title to display
+	 */
 	public void initLegend(String titleText);
 
+	/** Refreshes the chart legend. */
 	public void updateLegend();
 
+	/** Clears all benchmark data shown in the UI. */
 	public void resetTestData();
 
+	/**
+	 * Adds a read benchmark result to the chart.
+	 *
+	 * @param mark the read benchmark result
+	 */
 	public void addReadMark(DiskMark mark);
 
+	/**
+	 * Adds a write benchmark result to the chart.
+	 *
+	 * @param mark the write benchmark result
+	 */
 	public void addWriteMark(DiskMark mark);
 
+	/**
+	 * Adds a completed run to the run history panel.
+	 *
+	 * @param run the disk run to display
+	 */
 	public void addRun(DiskRun run);
 
+	/**
+	 * Shows a plain informational message dialog.
+	 *
+	 * @param msg   the message to display
+	 * @param title the dialog title
+	 */
 	public void sendPlainMessage(String msg, String title);
 
+	/**
+	 * Shows an error message dialog.
+	 *
+	 * @param msg   the error message to display
+	 * @param title the dialog title
+	 */
 	public void sendErrorMessage(String msg, String title);
 
+	/** Enables or disables UI controls based on whether a benchmark is running. */
 	public void adjustSensitivity();
 }
