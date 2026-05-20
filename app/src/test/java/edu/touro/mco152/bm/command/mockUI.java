7f4d0bbce79cdde8a3de3ace12bfa0ac39f57271
llamalectric <lotsnlotsofllamas@gmail.com>
2026-05-20 18:47:23 -0400

    Add command benchmarker tests and mock classes

---
+++ app/src/test/java/edu/touro/mco152/bm/command/mockUI.java
@@ -0,0 +1,63 @@
+package edu.touro.mco152.bm.command;
+
+import edu.touro.mco152.bm.DiskMark;
+import edu.touro.mco152.bm.DiskUI;
+import edu.touro.mco152.bm.persist.DiskRun;
+
+public class mockUI implements DiskUI {
+
+	@Override
+	public void addReadMark(DiskMark mark) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addRun(DiskRun run) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addWriteMark(DiskMark mark) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void adjustSensitivity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initLegend(String titleText) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resetTestData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendErrorMessage(String msg, String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendPlainMessage(String msg, String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateLegend() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
